0bebf34affe8ab05df8214115d9286a137341a32
sakirmd13 <mds079475@gmail.com>
2026-09-11 00:33:17 +0530

    Sum of Ascii value

---
+++ task7.cpp
@@ -0,0 +1,16 @@
+// Sum of Ascii value 
+#include<iostream>
+using namespace std;
+
+int main(){
+    char a= 'A';
+    int ascii = a;
+    cout<<ascii <<endl;
+
+
+    char A= 'a';
+    int ch = A;
+    cout<<ch <<endl;
+
+    cout<<ascii + ch <<endl;
+}
