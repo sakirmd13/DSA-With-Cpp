73e222a30ffdb9a1d638a2e9128abbcf67131b9f
sakirmd13 <mds079475@gmail.com>
2026-09-11 00:11:48 +0530

    simple intrest

---
+++ task3.cpp
@@ -0,0 +1,18 @@
+// Simple intrest
+#include <iostream>
+using namespace std;
+int main(){
+    float p,r,t, si;
+    cout<<"Enter the value of p: ";
+    cin>>p;
+
+    cout<<"Enter the value of r: ";
+    cin>>r;
+
+    cout<<"Enter the value of t: ";
+    cin>>t;
+
+    si =(p*r*t)/100;
+    cout<<"Simple intrest: "<<si;
+
+}
