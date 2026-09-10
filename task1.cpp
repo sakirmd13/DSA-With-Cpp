4f192742c63771d8f934b4bb008050b433a307e2
sakirmd13 <mds079475@gmail.com>
2026-09-11 00:04:35 +0530

    sum and mul

---
+++ task1.cpp
@@ -0,0 +1,12 @@
+// Square of a number
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter a number:"<<endl;
+   int n;
+   cin>>n;
+   cout<<n*n;
+   return 0;
+
+}
---
+++ task2.cpp
@@ -0,0 +1,12 @@
+// Sum of 2 number
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter 1st number: ";
+    cin>>a;
+    cout<<"Enter 2nd number: ";
+    cin>>b;
+    cout<<"Sum of 2 number: ";
+    cout<<a+b;
+}
