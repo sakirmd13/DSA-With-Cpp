2924d94bcafe3258fcafaf97605d6597b46fef2f
sakirmd13 <mds079475@gmail.com>
2026-09-12 16:21:39 +0530

    Add BoolDataType and RelationalOperators examples

---
+++ If-Else/BoolDataType.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool a = true;
+    int x=10;
+    int y=20;
+
+    cout << a << endl;
+    cout<<(2>4)<<endl;
+    cout<<(2<4)<<endl;
+    cout<<(x==y)<<endl;
+
+
+    
+}
