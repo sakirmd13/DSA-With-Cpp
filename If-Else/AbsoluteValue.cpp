455e7df071c69f4e218668e3f0f349522df1fe79
sakirmd13 <mds079475@gmail.com>
2026-09-12 16:59:58 +0530

    Refactor AbsoluteValue program to simplify output logic and focus on calculating absolute value

--- If-Else/AbsoluteValue.cpp
+++ If-Else/AbsoluteValue.cpp
@@ -6,10 +6,15 @@ int main()
     int num;
     cout << "Enter your number: ";
     cin >> num;
-    if(num <0 ){
-        cout <<num <<" is a negative number " <<-num <<endl;
-    }
-    else{
-        cout <<num <<" is a positive number " <<num <<endl;
-    }
+    // if (num < 0)
+    // {
+    //     cout << num << " is a negative number " << -num << endl;
+    // }
+    // else
+    // {
+    //     cout << num << " is a positive number " << num << endl;
+    // }
+    if (num < 0)
+        num *= -1;
+    cout << "Absolute value of " << num << " is " << num << endl;
 }
