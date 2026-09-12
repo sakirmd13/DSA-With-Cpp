8deb82f0579581e62774441a29306b6a2dcb4d06
sakirmd13 <mds079475@gmail.com>
2026-09-12 16:35:25 +0530

    Add PositiveInteger and ValidAgeForDriving examples

---
+++ If-Else/PositiveInteger.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<< "Enter you number: ";
+    cin>>number;
+    if (number%2 ==0){
+        cout<<number <<" is an even number "<<endl;
+        }
+    else{
+        cout <<number <<" is an odd number "<<endl;
+    }
+
+} 
---
+++ If-Else/ValidAgeForDriving.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout << "Enter your age: ";
+    cin>>age;
+    if (age>=18){
+        cout<<"You are eligible for driving"<<endl;
+    }
+    else{
+        cout<<"You are not eligible for driving"<<endl;
+    }
+}
